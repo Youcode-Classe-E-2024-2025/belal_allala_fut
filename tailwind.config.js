8f040b25fe10ec18daa96b3967ff74dcb201d4e0
belal-allala <belalallala810@gmail.com>
2024-11-26 12:54:53 +0100

    Initialisation du projet

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js}", // Cibler uniquement les fichiers HTML et JS dans le dossier src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
